57349c093a31ee70bbb119b08553e18197fdad72
Laura Pedroni <laurage_@hotmail.com>
2016-09-19 19:05:58 +0200

    mobile version ok

--- source/javascripts/hover_btn_process.js
+++ source/javascripts/hover_btn_process.js
@@ -1,86 +1,46 @@
-// The option arrow doesn't transform into a minus when mouseout.
+// For arrows in the process page - NOT USED
 
-$( document ).ready(function() {
-  var clicked = false;
-  changeColorOnHover("arrow");
-  changeSymbolOnClick();
+// $( document ).ready(function() {
+//   var clicked = false;
+//   changeColorOnHover("arrow");
+//   changeSymbolOnClick();
 
-  function changeSymbolOnClick()  {
-
-
-    $(".process-essential").click(function() {
-       clicked = !(clicked);
-
-      if(clicked == true){
-        var src = "/images/cercle-minus-darker.png";
-        $(this).attr("src", src);
-        changeColorOnHover("minus");
-      }
-
-      if(clicked == false){
-        var src = "/images/cercle-arrow-darker.png";
-        $(this).attr("src", src);
-        changeColorOnHover("arrow");
-      }
-
-    });
-
-    // $(".process-option").click(function() {
-    //    clicked = !(clicked);
-
-    //   if(clicked == true){
-    //     var src = "/images/cercle-minus.png";
-    //     $(this).attr("src", src);
-    //     changeColorOnHover("minus");
-
-    //   }
-
-    //   if(clicked == false){
-    //     var src = "/images/cercle-arrow.png";
-    //     $(this).attr("src", src);
-    //     changeColorOnHover("arrow");
-
-    //   }
-    //   console.log(clicked);
-    // });
-
-  };
-
-  function changeColorOnHover(symbol) {
-    $(".process-essential")
-        .mouseover(function() {
-            var src = "/images/cercle-"+symbol+"-darker.png";
-            $(this).attr("src", src);
-        })
-        .mouseout(function() {
-            var src = "/images/cercle-"+symbol+".png";
-            $(this).attr("src", src);
-        });
+//   function changeSymbolOnClick()  {
 
 
-    // $(".process-option")
-    //     .mouseover(function() {
-    //         var src = "/images/cercle-"+symbol+".png";
-    //         $(this).attr("src", src);
-    //     })
-    //     .mouseout(function() {
-    //         var src = "/images/cercle-"+symbol+"-lighter.png";
-    //         $(this).attr("src", src);
-    //     });
-  };
+//     $(".process-essential").click(function() {
+//        clicked = !(clicked);
+
+//       if(clicked == true){
+//         var src = "/images/cercle-minus-darker.png";
+//         $(this).attr("src", src);
+//         changeColorOnHover("minus");
+//       }
+
+//       if(clicked == false){
+//         var src = "/images/cercle-arrow-darker.png";
+//         $(this).attr("src", src);
+//         changeColorOnHover("arrow");
+//       }
+
+//     });
 
 
 
-//JS version:
-  // var clicked = false;
+//   };
 
-  // function changeIcon() {
-  //   clicked = !(clicked);
-  //   console.log(clicked);
-  // }
+//   function changeColorOnHover(symbol) {
+//     $(".process-essential")
+//         .mouseover(function() {
+//             var src = "/images/cercle-"+symbol+"-darker.png";
+//             $(this).attr("src", src);
+//         })
+//         .mouseout(function() {
+//             var src = "/images/cercle-"+symbol+".png";
+//             $(this).attr("src", src);
+//         });
 
-  // // Function to add event listener to clickable-element
-  // var clickable_element = document.getElementById("clickable-element");
-  // clickable_element.addEventListener("click", changeIcon, true);
 
-});
+//   };
+
+// });

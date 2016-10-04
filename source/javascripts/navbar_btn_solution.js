5c0d335a7c7f3eb06a4ad486a9260e8556fc4e61
hugo barthelemy <barthelemyhugo@gmail.com>
2016-10-04 14:56:21 +0200

    retour à l'index fonctionnel pour logo + solution mais pas de scroll

--- source/javascripts/navbar_btn_solution.js
+++ source/javascripts/navbar_btn_solution.js
@@ -2,6 +2,27 @@ $( document ).ready(function() {
   // btn logo
   $(".js-navbar-btn-logo").click(function(event) {
     console.log("click on js-navbar-btn-logo");
+
+    // check where you are
+    var pathname = window.location.pathname;
+    console.log(pathname);
+
+    // case where you aren't on index page
+    if((pathname != '/')&&(pathname != '/index.html')&&(pathname != '/en/index.html')){
+      console.log("on est dans le if");
+      // you are on /en legal notice, so send you to /en/index page before
+      if(pathname == '/en/legalnotice.html'){
+        console.log("mettre l'action d'aller à l'index en");
+        window.location.pathname = '/en/index.html';
+      }
+      // you are on /fr legal notice, so send you to /fr/index page before
+      else {
+        console.log("mettre l'action d'aller à l'index fr");
+        window.location.pathname = '/index.html';
+      }
+    }
+
+    // go to the good place in index
     $('html, body').animate({
       scrollTop: $(".head_of_landingpage").offset().top
        - 100}, 1000);
@@ -10,6 +31,28 @@ $( document ).ready(function() {
   // btn solution
   $(".navbar_btn_solution").click(function(event) {
     console.log("click on navbar_btn_solution");
+
+
+    // check where you are
+    var pathname = window.location.pathname;
+    console.log(pathname);
+
+    // case where you aren't on index page
+    if((pathname != '/')&&(pathname != '/index.html')&&(pathname != '/en/index.html')){
+      console.log("on est dans le if");
+      // you are on /en legal notice, so send you to /en/index page before
+      if(pathname == '/en/legalnotice.html'){
+        console.log("mettre l'action d'aller à l'index en");
+        window.location.pathname = '/en/index.html';
+      }
+      // you are on /fr legal notice, so send you to /fr/index page before
+      else {
+        console.log("mettre l'action d'aller à l'index fr");
+        window.location.pathname = '/index.html';
+      }
+    }
+
+    // go to the good place in index
     $('html, body').animate({
       scrollTop: $("#title_solution").offset().top-50
       }, 1000);
